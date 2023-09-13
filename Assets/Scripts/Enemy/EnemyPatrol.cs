52763a8db494bdf32cfce2365cc2fc7c09470161
TurboMindStudio <shivamchoudhari07@gmail.com>
2023-09-13 17:03:10 +0530

    adding enemy ai

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] Transform[] patrolPoint;
+    private NavMeshAgent enemyAi;
+    private int randomPos;
+    private Vector3 EnemyPos;
+    //private Transform player;
+
+
+    private void Start()
+    {
+        enemyAi = GetComponent<NavMeshAgent>();
+        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        EnemyPos = this.transform.position;
+
+        randomPos = Random.Range(0, patrolPoint.Length);
+        enemyAi.destination = patrolPoint[randomPos].position;
+    }
+
+    private void Update()
+    {
+     
+        float distanceBeetweenPoints = Vector3.Distance(transform.position, patrolPoint[randomPos].position);
+        Debug.Log(distanceBeetweenPoints);
+        if (distanceBeetweenPoints <= 5)
+        {
+            randomPos = Random.Range(0, patrolPoint.Length);
+            enemyAi.destination = patrolPoint[randomPos].position;
+        }
+        
+    }
+}
